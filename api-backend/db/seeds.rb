3fa9f6c74c905edfcf03ff37cb5fe9cb59f9dfa3
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-03-06 00:10:52 -0300

    Add seeds and refresh Gemfile

--- api-backend/db/seeds.rb
+++ api-backend/db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+license_plates = 'ABCDEF'.chars.permutation.to_a.map(&:join).sort[0...3]
+
+drivers = Driver.create([
+    {
+        first_name: 'Benjamin',
+        last_name: 'Silva',
+        identification_code: '1111111111',
+        phone: '1234567890'
+    },
+    {
+        first_name: 'Macarena',
+        last_name: 'Larrain',
+        identification_code: '2222222222',
+        phone: '2345678901'
+    },
+    {
+        first_name: 'Jorge',
+        last_name: 'Rodriguez',
+        identification_code: '3333333333',
+        phone: '3456789012'   
+    }    
+])
+
+devices = Device.create([
+    {
+        device_serial_number: 'd131dd02c5e6eec4'
+        adquisition_date: Time.now
+    },
+    {
+        device_serial_number: '4d131dd02c5e6eec'
+        adquisition_date: Time.now()
+    },
+    {
+        device_serial_number: 'ec4d131dd02c5e6e',
+        adquisition_date: Time.now()
+    }
+])
+
+
+vehicles = Vehicle.create([
+    {
+        license_plate: license_plates.pop(),
+        driver_id: 1
+        total_seats: (rand() * 100).to_i
+    }
+])
+
+vehicle_devices = VehicleDevice.create([
+    {
+        vehicle_id: 1,
+        driver_id: 1
+    },
+    {
+        vehicle_id: 2,
+        driver_id: 2
+    },
+    {
+        vehicle_id: 3,
+        driver_id: 3
+    }    
+])
