6fcc7b138d95c96b55b149b8922f06f47ba92c1f
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-03-08 12:34:50 -0300

    Refactor service and add routes coordinates

--- api-backend/app/services/distance_evaluator_service.rb
+++ api-backend/app/services/distance_evaluator_service.rb
@@ -1,21 +1,23 @@
 class DistanceEvaluatorService
+
   def self.distance_to_position(terminal_lat, terminal_lon, position_lat, position_lon, threshold = 100)
-    sql = "SELECT ST_Distance(
-            ST_Transform('SRID=4326;POINT(#{terminal_lon} #{terminal_lat})'::geometry, 2163),
-            ST_Transform('SRID=4326;POINT(#{position_lon} #{position_lat})'::geometry, 2163)
-        ) as distance"
-    Device.find_by_sql(sql).first
+    Device.find_by_sql(render_query(terminal_lat, terminal_lon, position_lat, position_lon)).first
   end
 
   def self.is_close_to_position?(terminal_lat, terminal_lon, position_lat, position_lon, threshold = 100)
-    sql = "SELECT ST_Distance(
-            ST_Transform('SRID=4326;POINT(#{terminal_lon} #{terminal_lat})'::geometry, 2163),
-            ST_Transform('SRID=4326;POINT(#{position_lon} #{position_lat})'::geometry, 2163)
-        ) as distance"
     begin
-      Device.find_by_sql(sql).first.distance <= threshold
+      Device.find_by_sql(render_query(terminal_lat, terminal_lon, position_lat, position_lon)).first.distance <= threshold
     rescue
         false
     end
+  end
+
+  private
+
+  def render_query(pterminal_lat, terminal_lon, position_lat, position_lon)
+    sql = "SELECT ST_Distance(
+            ST_Transform('SRID=4326;POINT(#{terminal_lon} #{terminal_lat})'::geometry, 2163),
+            ST_Transform('SRID=4326;POINT(#{position_lon} #{position_lat})'::geometry, 2163)
+        ) as distance"    
   end
 end
