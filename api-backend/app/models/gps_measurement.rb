f3099eec1b150398d8ddfcbb5a08d943f8b062c5
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-03-08 10:49:06 -0300

    Redefine distance calculation

--- api-backend/app/models/gps_measurement.rb
+++ api-backend/app/models/gps_measurement.rb
@@ -1,10 +1,6 @@
 class GpsMeasurement < ApplicationRecord
     belongs_to :device
-
-    scope :within, -> (latitude, longitude, distance_in_meters = 100) {
-        where(%{
-            ST_Distance(road_lonlat, 'POINT(%f %f)') < %d
-        } % [longitude, latitude, distance_in_meters])
-    }
+    
+    
 
 end
