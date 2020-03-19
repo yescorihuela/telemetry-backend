4b2954fdb5eb714fb7c468229546af297a1fdb79
Yrvin Escorihuela <yher136@gmail.com>
2020-03-19 07:10:16 -0300

    Delete conditional attributes

--- app/serializers/api/v1/gps_measurement_serializer.rb
+++ app/serializers/api/v1/gps_measurement_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::GpsMeasurementSerializer
   include FastJsonapi::ObjectSerializer
   set_id :id
-  attributes :incoming_measurement_at, :latitude, :longitude, :distance, :next_measurement, if: Proc.new {|record| !record.next_measurement.nil? and (record.next_measurement - record.incoming_measurement_at).to_i > 0 }
+  attributes :incoming_measurement_at, :latitude, :longitude, :distance, :next_measurement
 end
