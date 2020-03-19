923ab16c12149c744c26a9d9f22011ece58d3a13
Yrvin Escorihuela <yher136@gmail.com>
2020-03-19 07:06:27 -0300

    Fix typo and delete over-indentation

--- app/services/distance_evaluator_service.rb
+++ app/services/distance_evaluator_service.rb
@@ -3,8 +3,8 @@ class DistanceEvaluatorService
   def self.distance_to_position(point_from, point_to)
     begin
       Device.find_by_sql(%{SELECT ST_Distance(
-          ST_Transform('SRID=4326;%s'::geometry, 2163),
-          ST_Transform('SRID=4326;%s'::geometry, 2163)) as distance} % [point_from, point_to]).first[:distance]
+        ST_Transform('SRID=4326;%s'::geometry, 2163),
+        ST_Transform('SRID=4326;%s'::geometry, 2163)) as distance} % [point_from, point_to]).first[:distance]
     rescue
       -1
     end
@@ -13,8 +13,8 @@ class DistanceEvaluatorService
   def self.close_to_terminal?(point, threshold = 100)
       terminal = GeoFence.peor_es_nada_terminal()
       Device.find_by_sql(%{SELECT ST_Distance(
-          ST_Transform('SRID=4326;%s'::geometry, 2163),
-          ST_Transform('SRID=4326;%s'::geometry, 2163)) as distance} % [point, terminal.location]).first[:distance] <= threshold
+        ST_Transform('SRID=4326;%s'::geometry, 2163),
+        ST_Transform('SRID=4326;%s'::geometry, 2163)) as distance} % [point, terminal.location]).first[:distance] <= threshold
   end
 
   def self.departed_from(trip_id, device_id)
@@ -28,10 +28,10 @@ class DistanceEvaluatorService
     end
     departed_points = []
     terminals.each do |term|
-        distance = Device.find_by_sql(%{SELECT 
-        ST_Distance(ST_Transform('SRID=4326;%s'::geometry, 2163), 
-        ST_Transform('SRID=4326;%s'::geometry, 2163)) as distance} % [first_measurement, term[:location]]).first[:distance]
-        departed_points.push({:terminal_name => term.description, :distance_from_terminal => distance})
+      distance = Device.find_by_sql(%{SELECT 
+      ST_Distance(ST_Transform('SRID=4326;%s'::geometry, 2163), 
+      ST_Transform('SRID=4326;%s'::geometry, 2163)) as distance} % [first_measurement, term[:location]]).first[:distance]
+      departed_points.push({:terminal_name => term.description, :distance_from_terminal => distance})
     end
     departed_points.sort_by{|terminal, distance| distance}.first || -1
   end
